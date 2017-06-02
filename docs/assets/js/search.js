55b262a87317d788a99cb0e4d6a2df795e335d4a
Angelo Ocana <angeloocana@gmail.com>
2017-06-02 07:10:21 -0300

    Gen docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+﻿var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","32":"Variable"},"rows":[{"id":0,"kind":1,"name":"\"isNilOrEmpty\"","url":"modules/_isnilorempty_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":32,"name":"isNilOrEmpty","url":"modules/_isnilorempty_.html#isnilorempty","classes":"tsd-kind-variable tsd-parent-kind-external-module","parent":"\"isNilOrEmpty\""},{"id":2,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"}]};
