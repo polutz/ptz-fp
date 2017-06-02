ced1943b637a31cceafec70783ad8973b9e848ae
Angelo Ocana <angeloocana@gmail.com>
2017-06-02 07:08:24 -0300

    Update isNilOrEmpty jsdoc

--- dist-esnext/isNilOrEmpty.js
+++ dist-esnext/isNilOrEmpty.js
@@ -1,7 +1,7 @@
 import { anyPass, isEmpty, isNil } from 'ramda';
 /**
- * Checks if an array Is null or Empty
- * @param {*[]} value
+ * Checks if an array or string Is null or Empty.
+ * @param {string|*[]} value
  */
 export const isNilOrEmpty = anyPass([isNil, isEmpty]);
 //# sourceMappingURL=isNilOrEmpty.js.map
--- dist/isNilOrEmpty.js
+++ dist/isNilOrEmpty.js
@@ -8,8 +8,8 @@ exports.isNilOrEmpty = undefined;
 var _ramda = require('ramda');
 
 /**
- * Checks if an array Is null or Empty
- * @param {*[]} value
+ * Checks if an array or string Is null or Empty.
+ * @param {string|*[]} value
  */
 var isNilOrEmpty = exports.isNilOrEmpty = (0, _ramda.anyPass)([_ramda.isNil, _ramda.isEmpty]);
 //# sourceMappingURL=isNilOrEmpty.js.map
